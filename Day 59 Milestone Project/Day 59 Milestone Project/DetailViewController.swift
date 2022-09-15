725491413a90a707c3d4dc34e9ef823b03787c79
Al-Maqsat <maksat.balgabaev.00@gmail.com>
2022-09-15 22:05:15 +0600

    Added the Detail View

---
+++ Day 59 Milestone Project/Day 59 Milestone Project/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  Day 59 Milestone Project
+//
+//  Created by Maksat Baiserke on 15.09.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
