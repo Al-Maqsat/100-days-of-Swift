42e063247883ac9f300b3f99ab886907a24f8b89
Al-Maqsat <maksat.balgabaev.00@gmail.com>
2022-09-15 20:29:29 +0600

    added Day 59 milestone project Country

---
+++ Day 59 Milestone Project/Day 59 Milestone Project/Country.swift
@@ -0,0 +1,17 @@
+//
+//  Country.swift
+//  Day 59 Milestone Project
+//
+//  Created by Maksat Baiserke on 15.09.2022.
+//
+
+import Foundation
+
+struct Country: Codable{
+    var name: String?
+    var currency: String?
+    var capital: String?
+    var area: Double?
+    var population: Int?
+    var region: String?
+}
---
+++ Day 59 Milestone Project/Day 59 Milestone Project/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Day 59 Milestone Project
+//
+//  Created by Maksat Baiserke on 15.09.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var countries = [Country]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let urlString = "https://restcountries.com/v2/all"
+        
+        if let url = URL(string: urlString){
+            if let data = try? Data(contentsOf: url){
+                parse(json: data)
+            }
+        }
+    }
+    
+    func parse(json: Data){
+        let decoder = JSONDecoder()
+        
+        if let jsonInfo = try? decoder.decode([Country].self, from: json){
+            countries = jsonInfo
+//            tableView.reloadData()
+        }
+    }
+    
+    
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = countries[indexPath.row].name
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+}
+
