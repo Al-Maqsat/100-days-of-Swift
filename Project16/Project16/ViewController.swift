eee4be6920c894a345901ad03b325af9363e1108
Al-Maqsat <maksat.balgabaev.00@gmail.com>
2022-09-16 10:17:40 +0600

    Added several Captial city and also the Almaty out beloved City of the Apples

---
+++ Project16/Project16/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by Maksat Baiserke on 16.09.2022.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+    @IBOutlet var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics.")
+        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
+        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light.")
+        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
+        let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
+        let Nur_Sultan = Capital(title: "Nur-Sultan", coordinate: CLLocationCoordinate2D(latitude: 51.1605, longitude: 71.4704), info: "Hope will be renamed back to Astana")
+        let Almaty = Capital(title: "Almaty", coordinate: CLLocationCoordinate2D(latitude: 43.2220, longitude: 76.8512), info: "City of the apples")
+        
+        mapView.addAnnotations([london, oslo, paris, rome, washington, Nur_Sultan, Almaty])
+    }
+}
+
