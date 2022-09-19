249646389f978942ddbec8a73ca45f9f028a4858
Al-Maqsat <maksat.balgabaev.00@gmail.com>
2022-09-19 11:51:59 +0600

    Tested debegging methods window divider is the good one

---
+++ Project18Debugging/Project18Debugging/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project18Debugging
+//
+//  Created by Maksat Baiserke on 19.09.2022.
+//
+
+import UIKit
+ 
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        for i in  0...100{
+            print("Line \(i)")
+        }
+    }
+
+
+}
+
