7c7b5f443ddbf5671e49f6d082dadeedcc04a618
Al-Maqsat <maksat.balgabaev.00@gmail.com>
2022-10-12 18:55:38 +0600

    Basic view

--- Project20/Project20/GameScene.swift
+++ Project20/Project20/GameScene.swift
@@ -8,9 +8,55 @@
 import SpriteKit
 
 class GameScene: SKScene {
-
+    var gameTimer: Timer?
+    var fireworks = [SKNode]()
+    var ScoreLabel: SKLabelNode!
+    
+    let leftEdge = -22
+    let bottomEdge = -22
+    let rightEdge = 1024 + 22
+    
+    var score = 0 {
+        didSet{
+            ScoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    
     override func didMove(to view: SKView) {
+        ScoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        ScoreLabel.text = "Score: 0"
+        ScoreLabel.fontSize = 44
+        ScoreLabel.position = CGPoint(x: 16, y: 16)
+        ScoreLabel.horizontalAlignmentMode = .left
+        addChild(ScoreLabel)
         
+        let background = SKSpriteNode(imageNamed: "background")
+        background.zPosition = -1
+        background.blendMode = .replace
+        background.position = CGPoint(x: 512, y: 384)
+        addChild(background)
+        
+        gameTimer = Timer.scheduledTimer(timeInterval: 6, target: self, selector: #selector(launchFireworks), userInfo: nil, repeats: true)
+    }
+    
+    func createFirework(xMovement: CGFloat, x: Int, y: Int){
+        let node = SKNode()
+        node.position = CGPoint(x: x, y: y)
+        
+        let firework = SKSpriteNode(imageNamed: "rocket")
+        firework.colorBlendFactor = 1
+        firework.name = "firework"
+        node.addChild(firework)
+        
+        switch Int.random(in: 0...2){
+        case 0:
+            firework.color = .cyan
+        case 1:
+            firework.color = .green
+        default:
+            firework.color = .red
+        }
         
     }
 }
